58061118bc013bee24d365311d78e6d51eb952bc
s-damian <contact@damian-freelance.fr>
2024-05-04 14:20:20 +0300

    Refactoring struct MnemonicManager

--- src/app/mnemonic_manager.rs
+++ src/app/mnemonic_manager.rs
@@ -28,40 +28,44 @@ impl MnemonicManager {
 
         // Convertit la seed en un tableau de bytes bruts, qui servira de base pour la génération de clés dérivées.
         let seed_bytes = BipSeed::get_seed_bytes(&seed);
+        self.handle_key_derivation(seed_bytes);
+    }
 
+    /// Gère la dérivation de clés et leur enregistrement.
+    fn handle_key_derivation(&self, seed_bytes: &[u8]) {
         // Récupère le nombre de dérivations souhaitées (est de 1 par défaut).
         let nb_derivations = self.config.nb_derivations;
-        if nb_derivations == 1 {
-            // Génerer une paire de clés (clé publique et clé privée) à partir de la seed en bytes.
-            // Puis écrire cette paire de clés dans un fichier JSON.
-            let keypair = SolanaAddress::generate_keypair(seed_bytes);
-            let keypair_path = &self.config.keypair_path;
-            SolanaAddress::write_keypair(&keypair, keypair_path);
+        // Gère les dérivations multiples pour générer plusieurs paires de clés.
+        for index in 0..nb_derivations {
+            self.derive_and_store_keypair(seed_bytes, index);
+        }
+    }
 
-            // Affiche la clé publique (qui dans le cas de Solana, est également utilisée comme adresse publique du wallet).
-            println!("Solana Public Key: {}", keypair.pubkey());
-        } else {
-            // Gère les dérivations multiples pour générer plusieurs paires de clés.
-            for index in 0..nb_derivations {
-                // Dériver la seed pour chaque index spécifié (sauf pour l'index 0 qui utilise la seed originale).
-                match BipSeed::derive_seed_bytes(seed_bytes, index) {
-                    Ok(derived_seed_bytes) => {
-                        // Génerer une paire de clés (clé publique et clé privée) à partir de la seed en bytes.
-                        // Puis écrire cette paire de clés dans un fichier JSON.
-                        let keypair = SolanaAddress::generate_keypair(&derived_seed_bytes);
-                        let keypair_path = if index == 0 {
-                            self.config.keypair_path.clone() // (cloner pour éviter le mouvement).
-                        } else {
-                            format!("{}/keypair-{}.json", self.config.keypair_dir, index)
-                        };
-                        SolanaAddress::write_keypair(&keypair, &keypair_path);
+    /// Dérive et stocke une paire de clés.
+    fn derive_and_store_keypair(&self, seed_bytes: &[u8], index: usize) {
+        // Dériver la seed pour chaque index spécifié (sauf pour l'index 0 qui utilise la seed originale).
+        match BipSeed::derive_seed_bytes(seed_bytes, index) {
+            Ok(derived_seed_bytes) => {
+                // Génerer une paire de clés (clé publique et clé privée) à partir de la seed en bytes.
+                // Puis écrire cette paire de clés dans un fichier JSON.
+                let keypair = SolanaAddress::generate_keypair(&derived_seed_bytes);
 
-                        // Affiche la clé publique (qui dans le cas de Solana, est également utilisée comme adresse publique du wallet).
-                        println!("Solana Public Key {}: {}", index, keypair.pubkey());
-                    }
-                    Err(e) => println!("Error deriving seed bytes: {}", e),
-                }
+                let keypair_path = self.derive_keypair_path(index);
+                SolanaAddress::write_keypair(&keypair, &keypair_path);
+
+                // Affiche la clé publique (qui dans le cas de Solana, est également utilisée comme adresse publique du wallet).
+                println!("Solana Public Key {}: {}", index, keypair.pubkey());
             }
+            Err(e) => println!("Error deriving seed bytes: {}", e),
+        }
+    }
+
+    /// Construit le chemin du fichier de la paire de clés en fonction de l'index de dérivation.
+    fn derive_keypair_path(&self, index: usize) -> String {
+        if index == 0 {
+            self.config.keypair_path.clone()
+        } else {
+            format!("{}/keypair-{}.json", self.config.keypair_dir, index)
         }
     }
 }
