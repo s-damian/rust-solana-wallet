03daa7074e1478cb85dfd17e5913042cf47696d1
s-damian <contact@damian-freelance.fr>
2024-05-04 13:30:48 +0300

    Init struct MnemonicManager

--- src/app/mnemonic_manager.rs
+++ src/app/mnemonic_manager.rs
@@ -5,52 +5,64 @@ use crate::solana::address::{generate_keypair, write_keypair};
 use bip39::Mnemonic;
 use solana_sdk::signer::Signer;
 
-/// Traite une mnémonique pour générer et afficher la clé publique correspondante, en prenant en compte les dérivations spécifiées.
-/// Cette fonction sert de point central pour la création de clés Solana à partir d'une phrase mnémonique.
-pub fn process_mnemonic(mnemonic: &Mnemonic, wallet_config: &WalletConfig) {
-    // Demande à l'utilisateur d'entrer une passphrase optionnelle qui sera utilisée lors de la génération de la seed.
-    // (laisser vide pour ne pas utiliser de passphrase)
-    let passphrase = prompt_for_passphrase();
+pub struct MnemonicManager {
+    config: WalletConfig,
+}
 
-    // Génère une seed en format hexadécimal à partir de la phrase mnémonique et de la passphrase.
-    // Cette seed de portefeuille HD (Hiérarchiquement Déterministe) permettra de produire une suite cohérente de clés dérivées.
-    let seed = generate_seed(mnemonic, &passphrase);
-    println!("Seed: {:X}", seed);
+impl MnemonicManager {
+    pub fn new(config: WalletConfig) -> Self {
+        Self { config }
+    }
 
-    // Convertit la seed en un tableau de bytes bruts, qui servira de base pour la génération de clés dérivées.
-    let seed_bytes = get_seed_bytes(&seed);
+    /// Traite une mnémonique pour générer et afficher la clé publique correspondante, en prenant en compte les dérivations spécifiées.
+    /// Cette fonction sert de point central pour la création de clés Solana à partir d'une phrase mnémonique.
+    pub fn process_mnemonic(&self, mnemonic: &Mnemonic) {
+        let wallet_config = &self.config;
 
-    // Récupère le nombre de dérivations souhaitées (est de 1 par défaut).
-    let nb_derivations = wallet_config.nb_derivations;
-    if nb_derivations == 1 {
-        // Génerer une paire de clés (clé publique et clé privée) à partir de la seed en bytes.
-        // Puis écrire cette paire de clés dans un fichier JSON.
-        let keypair = generate_keypair(seed_bytes);
-        let keypair_path = &wallet_config.keypair_path;
-        write_keypair(&keypair, keypair_path);
+        // Demande à l'utilisateur d'entrer une passphrase optionnelle qui sera utilisée lors de la génération de la seed.
+        // (laisser vide pour ne pas utiliser de passphrase)
+        let passphrase = prompt_for_passphrase();
 
-        // Affiche la clé publique (qui dans le cas de Solana, est également utilisée comme adresse publique du wallet).
-        println!("Solana Public Key: {}", keypair.pubkey());
-    } else {
-        // Gère les dérivations multiples pour générer plusieurs paires de clés.
-        for index in 0..nb_derivations {
-            // Dériver la seed pour chaque index spécifié (sauf pour l'index 0 qui utilise la seed originale).
-            match derive_seed_bytes(seed_bytes, index) {
-                Ok(derived_seed_bytes) => {
-                    // Génerer une paire de clés (clé publique et clé privée) à partir de la seed en bytes.
-                    // Puis écrire cette paire de clés dans un fichier JSON.
-                    let keypair = generate_keypair(&derived_seed_bytes);
-                    let keypair_path = if index == 0 {
-                        wallet_config.keypair_path.clone() // (cloner pour éviter le mouvement).
-                    } else {
-                        format!("{}/keypair-{}.json", &wallet_config.keypair_dir, index)
-                    };
-                    write_keypair(&keypair, &keypair_path);
+        // Génère une seed en format hexadécimal à partir de la phrase mnémonique et de la passphrase.
+        // Cette seed de portefeuille HD (Hiérarchiquement Déterministe) permettra de produire une suite cohérente de clés dérivées.
+        let seed = generate_seed(mnemonic, &passphrase);
+        println!("Seed: {:X}", seed);
 
-                    // Affiche la clé publique (qui dans le cas de Solana, est également utilisée comme adresse publique du wallet).
-                    println!("Solana Public Key {}: {}", index, keypair.pubkey());
+        // Convertit la seed en un tableau de bytes bruts, qui servira de base pour la génération de clés dérivées.
+        let seed_bytes = get_seed_bytes(&seed);
+
+        // Récupère le nombre de dérivations souhaitées (est de 1 par défaut).
+        let nb_derivations = wallet_config.nb_derivations;
+        if nb_derivations == 1 {
+            // Génerer une paire de clés (clé publique et clé privée) à partir de la seed en bytes.
+            // Puis écrire cette paire de clés dans un fichier JSON.
+            let keypair = generate_keypair(seed_bytes);
+            let keypair_path = &wallet_config.keypair_path;
+            write_keypair(&keypair, keypair_path);
+
+            // Affiche la clé publique (qui dans le cas de Solana, est également utilisée comme adresse publique du wallet).
+            println!("Solana Public Key: {}", keypair.pubkey());
+        } else {
+            // Gère les dérivations multiples pour générer plusieurs paires de clés.
+            for index in 0..nb_derivations {
+                // Dériver la seed pour chaque index spécifié (sauf pour l'index 0 qui utilise la seed originale).
+                match derive_seed_bytes(seed_bytes, index) {
+                    Ok(derived_seed_bytes) => {
+                        // Génerer une paire de clés (clé publique et clé privée) à partir de la seed en bytes.
+                        // Puis écrire cette paire de clés dans un fichier JSON.
+                        let keypair = generate_keypair(&derived_seed_bytes);
+                        let keypair_path = if index == 0 {
+                            wallet_config.keypair_path.clone() // (cloner pour éviter le mouvement).
+                        } else {
+                            format!("{}/keypair-{}.json", &wallet_config.keypair_dir, index)
+                        };
+                        write_keypair(&keypair, &keypair_path);
+
+                        // Affiche la clé publique (qui dans le cas de Solana, est également utilisée comme adresse publique du wallet).
+                        println!("Solana Public Key {}: {}", index, keypair.pubkey());
+                    }
+                    Err(e) => println!("Error deriving seed bytes: {}", e),
                 }
-                Err(e) => println!("Error deriving seed bytes: {}", e),
             }
         }
     }
--- src/app/wallet_manager.rs
+++ src/app/wallet_manager.rs
@@ -1,4 +1,4 @@
-use crate::app::mnemonic_manager::process_mnemonic;
+use crate::app::mnemonic_manager::MnemonicManager;
 use crate::bip::mnemonic::{generate_mnemonic, get_mnemonic_from_phrase, get_mnemonic_to_str};
 use crate::config::wallet_config::WalletConfig;
 
@@ -14,6 +14,8 @@ impl WalletManager {
     /// Génère une phrase mnémonique aléatoire de 12 mots et affiche la clé publique Solana correspondante.
     /// Cette fonction est typiquement utilisée pour la création initiale d'un portefeuille.
     pub fn generate_and_print_random_mnemonic(&self) {
+        let mnemonic_manager = MnemonicManager::new(self.config.clone());
+
         // Créer une nouvelle phrase mnémonique générée aléatoirement (en suivant le standard BIP39).
         // Il s'agit d'une mnémonique de 12 mots, ce qui est un standard commun pour de nombreux portefeuilles.
         let mnemonic = generate_mnemonic();
@@ -24,18 +26,20 @@ impl WalletManager {
 
         println!("BIP39 Mnemonic (random phrase): {}", phrase);
 
-        process_mnemonic(&mnemonic, &self.config);
+        mnemonic_manager.process_mnemonic(&mnemonic);
     }
 
     /// Génère une phrase mnémonique à partir d'une phrase donnée (12/24/Etc. mots) et affiche la clé publique Solana correspondante.
     /// Cette méthode permet d'utiliser une phrase existante pour récupérer ou accéder à un portefeuille.
     pub fn generate_and_print_mnemonic_from_phrase(&self, phrase: &str) {
+        let mnemonic_manager = MnemonicManager::new(self.config.clone());
+
         println!("BIP39 Mnemonic (given phrase): {}", phrase);
 
         // Convertit la chaîne de caractères fournie en une structure mnémonique valide.
         // Cette étape vérifie que la phrase correspond aux critères du standard BIP39 et qu'elle peut être utilisée pour générer une seed.
         let mnemonic = get_mnemonic_from_phrase(phrase);
 
-        process_mnemonic(&mnemonic, &self.config);
+        mnemonic_manager.process_mnemonic(&mnemonic);
     }
 }
